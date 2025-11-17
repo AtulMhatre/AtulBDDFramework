980423185f23638696e93700649c3e976669939d
Atul <you@example.com>
2025-11-18 01:49:26 +0530

    new branch added and code added

--- src/main/java/org/BDD/Test1/Atul3.java
+++ src/main/java/org/BDD/Test1/Atul3.java
@@ -2,5 +2,5 @@ package org.BDD.Test1;
 
 public class Atul3 {
 
-    String name1= "Sonal";      
+    String name1= "Sonal";
 }
---
+++ src/main/java/org/BDD/Test1/F1Branch1.java
@@ -0,0 +1,4 @@
+package org.BDD.Test1;
+
+public class F1Branch1 {
+}
